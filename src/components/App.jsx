03d9cce8633806ec5ef73f28095bbee37e3f2933
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-05-15 10:36:04 +0300

    create routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        // height: '100vh',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      React homework template
+      }}>
+      <ul>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/movies'>Movies</NavLink></li>
+        <li><NavLink to='/movies/:movieId'>MovieDetails</NavLink></li>
+        <li><NavLink to='/movies/:movieId/cast'>Cast</NavLink></li>
+        <li><NavLink to='/movies/:movieId/reviews'>Reviews</NavLink></li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<div>Home</div>} />
+        <Route path='/movies' element={<div>Movies</div>} />
+        <Route path='/movies/:movieId' element={<div>MovieDetails</div>} />
+        <Route path='/movies/:movieId/cast' element={<div>Cast</div>} />
+        <Route path='/movies/:movieId/reviews' element={<div>Reviews</div>} />
+      </Routes>
     </div>
   );
 };
