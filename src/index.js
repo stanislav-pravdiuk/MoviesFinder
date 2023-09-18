5992658abf25458d9f8c9885da9808dfc32cdc45
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-09-18 13:19:36 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies'>
+    <BrowserRouter basename='/MoviesFinder'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
